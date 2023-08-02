e9289396599765e423b0314ab58f04dab791650b
esn510sk <140237910+esn510sk@users.noreply.github.com>
2023-08-03 00:09:58 +0530

    Add files via upload

---
+++ assignment1.m
@@ -0,0 +1,19 @@
+clear all
+close all
+a=60;
+b=80;
+c=50;
+d=100;
+e=input("wnte value e\n");
+f=input("write value f\n");
+g=input("write value g\n");
+h=input("write value h\n");
+x=a+e;
+fprintf("%d + %d =%d\n",a,e,x);
+z=b-f;
+fprintf("%d - %d = %d\n",b,f,z);
+s=c*g;
+fprintf("%d * %d=%d\n",c,g,s);
+t=d/h;
+fprintf("%d/%d=%d\n",d,h,t);
+ 
